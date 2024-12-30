d0290bf6f81be46980474dcec86528645efe9f58
ShahhNasir <shahhnasir83@gmail.com>
2024-12-30 11:25:28 +0530

    Created template for my project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nasir Hussain",
+    author_email="shahhnasir83@gmail.com",
+    packages=find_packages()
+)
